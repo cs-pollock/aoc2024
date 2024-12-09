8fb5d1ce077647c02426749e6f13191f04311b19
cs-pollock <akira.pollock@gmail.com>
2024-12-09 23:16:55 +0100

    Solve Two A

--- aoc2024/02/Two.cs
+++ aoc2024/02/Two.cs
@@ -1,9 +1,34 @@
+using aoc2024.utils;
+
 namespace aoc2024;
 
 public class Two()
 {
     public static int RunA(string input)
     {
-        return 0;
+        var lines = Utils.SplitByLines(input);
+        var reports = lines.Select(line => line.Split(' ').Select(int.Parse));
+
+        return reports.Aggregate(0, (result, report) =>
+            IsReportSafe(report.ToArray()) ? ++result : result
+        );
+    }
+
+    public static bool IsReportSafe(int[] report)
+    {
+        var isPositive = false;
+        for (int i = 0; i < report.Count() - 1; i++)
+        {
+            var currentDistance = report[i] - report[i + 1];
+            if (i == 0)
+                isPositive = int.IsPositive(currentDistance);
+
+            if (int.Abs(currentDistance) > 3
+                || int.Abs(currentDistance) == 0
+                || isPositive != int.IsPositive(currentDistance))
+                return false;
+        }
+
+        return true;
     }
 }
--- aoc2024/02/TwoTest.cs
+++ aoc2024/02/TwoTest.cs
@@ -8,9 +8,22 @@ public class TwoTest
     private const string INPUT_LOCATION = """D:\code\aoc2024\aoc2024\02\input.txt""";
 
     [TestMethod]
+    [DataRow(new int[] {7, 6, 4, 2, 1}, true)]
+    [DataRow(new int[] {1, 2, 7, 8, 9}, false)]
+    [DataRow(new int[] {9, 7, 6, 2, 1}, false)]
+    [DataRow(new int[] {1, 3, 2, 4, 5}, false)]
+    [DataRow(new int[] {8, 6, 4, 4, 1}, false)]
+    [DataRow(new int[] {1, 3, 6, 7, 9}, true)]
+    public void TestIsReportSafe(int[] report, bool IsReportSafe)
+    {
+        Assert.AreEqual(IsReportSafe, Two.IsReportSafe(report));
+    }
+
+    [TestMethod]
     public void SolveA()
     {
         var solution = Two.RunA(Utils.GetInputFromFile(INPUT_LOCATION));
+        Assert.AreEqual(479, solution);
     }
 
     [TestMethod]
