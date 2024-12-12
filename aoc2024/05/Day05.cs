d81bbbbdac8a40086fe45ae901d111a33da33b69
cs-pollock <akira.pollock@gmail.com>
2024-12-12 09:42:26 +0100

    Solve Day05 A

--- aoc2024/05/Day05.cs
+++ aoc2024/05/Day05.cs
@@ -1,10 +1,54 @@
+using aoc2024.utils;
+
 namespace aoc2024;
 
 public class Day05
 {
     public static int RunA(string input)
     {
-        return 0;
+        var (rules, updates) = ParseInput(input);
+
+        var cannotGoBefore = rules.GroupBy(rule => rule[0], rule => rule[1])
+            .ToDictionary(g => g.Key, g => g.ToList());
+        var cannotGoAfter = rules.GroupBy(rule => rule[1], rule => rule[0])
+            .ToDictionary(g => g.Key, g => g.ToList());
+
+        List<int[]> validUpdates = [];
+        foreach(var update in updates)
+        {
+            for (int i = 0; i < update.Length; i++)
+            {
+                int currentNumber = update[i];
+                int[] numbersBefore = i == 0 ? [] : new ArraySegment<int>(update, 0, i).ToArray();
+                int[] numbersAfter = i == update.Length - 1 ? [] : new ArraySegment<int>(update, i + 1, update.Length - (i + 1)).ToArray();
+
+                if(cannotGoBefore.ContainsKey(currentNumber) && numbersBefore.Any(cannotGoBefore[currentNumber].Contains))
+                    break;
+
+                if(cannotGoAfter.ContainsKey(currentNumber) && numbersAfter.Any(cannotGoAfter[currentNumber].Contains))
+                    break;
+
+                if (i == update.Length - 1)
+                    validUpdates.Add(update);
+            }
+        }
+
+        return validUpdates.Aggregate(0, (result, update) => result + update[update.Length / 2]);
+    }
+
+    private static (int[][] rules, int[][]updates) ParseInput(string input)
+    {
+        string[] lines = input.Split(["\r\n\r\n"], StringSplitOptions.RemoveEmptyEntries);
+        var rules = Utils.SplitByLines(lines[0])
+            .Select(rule => rule.Split('|')
+            .Select(int.Parse).ToArray())
+            .ToArray();
+        var updates = Utils.SplitByLines(lines[1])
+            .Select(update => update.Split(',')
+            .Select(int.Parse).ToArray())
+            .ToArray();
+
+        return (rules, updates);
     }
 
     public static int RunB(string input)
--- aoc2024/05/Day05Test.cs
+++ aoc2024/05/Day05Test.cs
@@ -6,12 +6,20 @@ namespace aoc2024;
 public class Day05Test
 {
     private const string INPUT_LOCATION = """D:\code\aoc2024\aoc2024\05\input.txt""";
+    private const string TEST_INPUT_LOCATION = """D:\code\aoc2024\aoc2024\05\testInput.txt""";
+
+    [TestMethod]
+    public void TestA()
+    {
+        var solution = Day05.RunA(Utils.GetInputFromFile(TEST_INPUT_LOCATION));
+        Assert.AreEqual(143, solution);
+    }
 
     [TestMethod]
     public void SolveA()
     {
         var solution = Day05.RunA(Utils.GetInputFromFile(INPUT_LOCATION));
-        Assert.AreEqual(0, solution);
+        Assert.AreEqual(4959, solution);
     }
 
     [TestMethod]
