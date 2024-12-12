e6e3284fdb592be8336839c229d2fed3c21a376f
cs-pollock <akira.pollock@gmail.com>
2024-12-12 09:51:57 +0100

    Extract sorting fn for Day05

--- aoc2024/05/Day05.cs
+++ aoc2024/05/Day05.cs
@@ -6,6 +6,13 @@ public class Day05
 {
     public static int RunA(string input)
     {
+        return SortUpdates(input)
+            .valid
+            .Aggregate(0, (result, update) => result + update[update.Length / 2]);
+    }
+
+    private static (List<int[]> valid, List<int[]> invalid) SortUpdates(string input)
+    {
         var (rules, updates) = ParseInput(input);
 
         var cannotGoBefore = rules.GroupBy(rule => rule[0], rule => rule[1])
@@ -14,26 +21,39 @@ public class Day05
             .ToDictionary(g => g.Key, g => g.ToList());
 
         List<int[]> validUpdates = [];
-        foreach(var update in updates)
+        List<int[]> invalidUpdates = [];
+        foreach (var update in updates)
         {
+            bool isValid = false;
             for (int i = 0; i < update.Length; i++)
             {
                 int currentNumber = update[i];
                 int[] numbersBefore = i == 0 ? [] : new ArraySegment<int>(update, 0, i).ToArray();
                 int[] numbersAfter = i == update.Length - 1 ? [] : new ArraySegment<int>(update, i + 1, update.Length - (i + 1)).ToArray();
 
-                if(cannotGoBefore.ContainsKey(currentNumber) && numbersBefore.Any(cannotGoBefore[currentNumber].Contains))
+                if (cannotGoBefore.ContainsKey(currentNumber) && numbersBefore.Any(cannotGoBefore[currentNumber].Contains))
+                {
+                    isValid = false;
                     break;
+                }
 
-                if(cannotGoAfter.ContainsKey(currentNumber) && numbersAfter.Any(cannotGoAfter[currentNumber].Contains))
+                if (cannotGoAfter.ContainsKey(currentNumber) && numbersAfter.Any(cannotGoAfter[currentNumber].Contains))
+                {
+                    isValid = false;
                     break;
+                }
 
                 if (i == update.Length - 1)
                     validUpdates.Add(update);
             }
+
+            if (isValid)
+                validUpdates.Add(update);
+            else
+                invalidUpdates.Add(update);
         }
 
-        return validUpdates.Aggregate(0, (result, update) => result + update[update.Length / 2]);
+        return (validUpdates, invalidUpdates);
     }
 
     private static (int[][] rules, int[][]updates) ParseInput(string input)
@@ -53,6 +73,8 @@ public class Day05
 
     public static int RunB(string input)
     {
+        (_, var invalidUpdates) = SortUpdates(input);
+
         return 0;
     }
 }
